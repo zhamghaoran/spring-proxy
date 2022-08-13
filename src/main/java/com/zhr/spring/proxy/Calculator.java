287b899836ee5b552e4dadb96ead18edf835bb9d
zhanghaoran <2017929959@qq.com>
2022-08-13 17:52:27 +0800

    Initial commit

---
+++ src/main/java/com/zhr/spring/proxy/Calculator.java
@@ -0,0 +1,2 @@
+package com.zhr.spring.proxy;public interface Caculator {
+}
---
+++ src/main/java/com/zhr/spring/proxy/CalculatorStaticProxy.java
@@ -0,0 +1,23 @@
+package com.zhr.spring.proxy;
+
+public class CaculatorStaticProxy implements Calculator{
+    @Override
+    public int add(int i, int j) {
+        return 0;
+    }
+
+    @Override
+    public int sub(int i, int j) {
+        return 0;
+    }
+
+    @Override
+    public int mul(int i, int j) {
+        return 0;
+    }
+
+    @Override
+    public int div(int i, int j) {
+        return 0;
+    }
+}
